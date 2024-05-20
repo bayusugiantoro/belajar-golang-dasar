bfc30e5f5aefb6368975ff0b3c1783159aa0780c
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-05-20 12:50:39 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	name := "Bayu S"
+	fmt.Println(name)
+
+	name = "Bayu Sugiantoro"
+	fmt.Println(name)
+
+	var (
+		firstName  = "Bayu"
+		lastName   = "Sugiantoro"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
