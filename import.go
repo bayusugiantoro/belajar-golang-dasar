198907be507b09b9f8c76f5dae630d7b5597f473
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-05-25 20:54:11 +0700

    access modifier

--- import.go
+++ import.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
-	result := helper.SayHello("Bayu")
+	result := helper.SayHello("Eko")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // tidak bisa diakses
+	// fmt.Println(helper.sayGoodBye("Eko")) // tidak bisa diakses
 }
